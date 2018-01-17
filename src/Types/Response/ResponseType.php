b90e99e58568bad853b6b4171f926eb0fa03e33c
Timon Bolier <timonb@gmail.com>
2018-01-17 12:42:22 +0100

    Add protocol types constants

---
+++ src/Types/Response/ResponseType.php
@@ -0,0 +1,45 @@
+<?php
+namespace TBolier\RethinkQL\Types\Response;
+
+class ResponseType
+{
+    /**
+     * @var int
+     */
+    public const SUCCESS_ATOM = 1;
+
+    /**
+     * @var int
+     */
+    public const SUCCESS_SEQUENCE = 2;
+
+    /**
+     * @var int
+     */
+    public const SUCCESS_PARTIAL = 3;
+
+    /**
+     * @var int
+     */
+    public const WAIT_COMPLETE = 4;
+
+    /**
+     * @var int
+     */
+    public const SERVER_INFO = 5;
+
+    /**
+     * @var int
+     */
+    public const CLIENT_ERROR = 16;
+
+    /**
+     * @var int
+     */
+    public const COMPILE_ERROR = 17;
+
+    /**
+     * @var int
+     */
+    public const RUNTIME_ERROR = 18;
+}
