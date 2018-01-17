7c64117c055980dbf5f3c76a18dc9341cea43690
Timon Bolier <timonb@gmail.com>
2018-01-17 12:42:22 +0100

    Add protocol types constants

---
+++ src/Types/Query/QueryType.php
@@ -0,0 +1,30 @@
+<?php
+namespace TBolier\RethinkQL\Types\Query;
+
+class QueryType
+{
+    /**
+     * @var int
+     */
+    public const START = 1;
+
+    /**
+     * @var int
+     */
+    public const CONTINUE = 2;
+
+    /**
+     * @var int
+     */
+    public const STOP = 3;
+
+    /**
+     * @var int
+     */
+    public const NOREPLY_WAIT = 4;
+
+    /**
+     * @var int
+     */
+    public const SERVER_INFO = 5;
+}
