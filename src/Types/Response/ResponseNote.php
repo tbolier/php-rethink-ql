7a69be30558c4146434ccb749bb819bbfa9b18ac
Timon Bolier <timonb@gmail.com>
2018-01-18 11:48:45 +0100

    Add the remainder of ReQL types

---
+++ src/Types/Response/ResponseNote.php
@@ -0,0 +1,30 @@
+<?php
+namespace TBolier\RethinkQL\Types\Response;
+
+class ResponseNote
+{
+    /**
+     * @var int
+     */
+    public const SEQUENCE_FEED = 1;
+
+    /**
+     * @var int
+     */
+    public const ATOM_FEED = 2;
+
+    /**
+     * @var int
+     */
+    public const ORDER_BY_LIMIT_FEED = 3;
+
+    /**
+     * @var int
+     */
+    public const UNIONED_FEED = 4;
+
+    /**
+     * @var int
+     */
+    public const INCLUDES_STATES = 5;
+}
