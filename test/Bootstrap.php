674120f8e14f0135a85096190f42d7367eb69725
Timon Bolier <timonb@gmail.com>
2018-01-24 11:09:40 +0100

    Add scrutinizer support for integration tests

--- test/Bootstrap.php
+++ test/Bootstrap.php
@@ -4,4 +4,10 @@ declare(strict_types=1);
 mb_internal_encoding('UTF-8');
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/config.php';
+
+$configFile =  __DIR__ . '/config.php';
+if (getenv('integration_test_file')) {
+    $configFile =  __DIR__ . '/config_scrutinizer.php';
+}
+
+require_once $configFile;
