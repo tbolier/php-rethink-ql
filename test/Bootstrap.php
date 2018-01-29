1041bc1cc418053093ed652ac3b93e202a77fd34
AxaliaN <michel@michelmaas.com>
2018-01-29 08:59:45 +0100

    Reverted change to Bootstrap.php

--- test/Bootstrap.php
+++ test/Bootstrap.php
@@ -2,14 +2,8 @@
 declare(strict_types=1);
 
 mb_internal_encoding('UTF-8');
-chdir(dirname(__DIR__));
 
-$vendorPath = findParentPath('vendor');
-if (is_readable($vendorPath . '/autoload.php')) {
-    $loader = include($vendorPath . '/autoload.php');
-} else {
-    throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
-}
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $configFile =  __DIR__ . '/config.php';
 if (getenv('integration_test_file')) {
@@ -17,24 +11,3 @@ if (getenv('integration_test_file')) {
 }
 
 require_once $configFile;
-
-
-/**
- * Search upwards for a certain directory
- *
- * @param string $path
- * @return string
- */
-function findParentPath($path)
-{
-    $dir = __DIR__;
-    $previousDir = '.';
-    while (!is_dir($dir . '/' . $path)) {
-        $dir = dirname($dir);
-        if ($previousDir === $dir) {
-            return false;
-        }
-        $previousDir = $dir;
-    }
-    return $dir . '/' . $path;
-}
