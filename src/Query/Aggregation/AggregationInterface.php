c61b923937ad4fd3e6a616e754bea8d4e46f0ef8
Marc Geurts <geurtsmarc@hotmail.com>
2018-02-15 09:52:33 +0100

    Fix last issues

--- src/Query/Aggregation/AggregationInterface.php
+++ src/Query/Aggregation/AggregationInterface.php
@@ -3,6 +3,12 @@ declare(strict_types = 1);
 
 namespace TBolier\RethinkQL\Query\Aggregation;
 
+use TBolier\RethinkQL\Response\ResponseInterface;
+
 interface AggregationInterface
 {
+    /**
+     * @return Iterable|ResponseInterface
+     */
+    public function run();
 }
